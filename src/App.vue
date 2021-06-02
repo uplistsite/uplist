073beb1867b68112a0ee8000c688ce3d3da95c99
Tristan Martin <uplistsite@gmail.com>
2021-06-02 15:46:09 -0500

    Made navbar collapse on smaller screens

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-sm navbar-light bg-light">
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
     <div class="container-fluid">
       <router-link to="/">
         <a class="navbar-brand">Uplist.io</a>
